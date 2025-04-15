91741cd2e70441015dc2b535702602b91c7cacd8
rioadityapratama <rioaditya444@gmail.com>
2025-04-15 14:01:06 +0800

    membuat method GET untuk mendapatkan semua produk dan membuat GET BY ID untuk mendapatkan produk dengan id

--- routes/search_routes.go
+++ routes/search_routes.go
@@ -9,5 +9,19 @@ import (
 )
 
 func Search(router *gin.Engine, db *gorm.DB) {
+	router.GET("/search", func(c *gin.Context) {
+		var search []models.Search
+		db.Preload("Product").Find(&search)
+		c.JSON(http.StatusOK, gin.H{"data": search})
+	})
+
+	router.GET("/search/:id", func(c *gin.Context) {
+		var search models.Search
+		if err := db.Preload("Product").First(&search, "id_search = ?", c.Param("id")).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"message": "Search not found"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"data": search})
+	})
 
 }
