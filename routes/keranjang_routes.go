cc2af9dc7f80cf0ea39b91f9018662655481c183
rioadityapratama <rioaditya444@gmail.com>
2025-04-15 14:33:35 +0800

    menambahkan  method GET BY ID

--- routes/keranjang_routes.go
+++ routes/keranjang_routes.go
@@ -31,6 +31,30 @@ func Keranjang(router *gin.Engine, db *gorm.DB) {
 
 		c.JSON(http.StatusOK, gin.H{"data": result})
 	})
+
+	router.GET("/keranjang/:id", func(c *gin.Context) {
+		var keranjang models.Keranjang
+		if err := db.Preload("Product").Preload("User").First(&keranjang, "id_keranjang = ?", c.Param("id")).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"message": "Keranjang not found"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": gin.H{
+				"id_keranjang": keranjang.IDKeranjang,
+				"id_product":   keranjang.IDProduct,
+				"id_user":      keranjang.IDUser,
+				"jumlah":       keranjang.Jumlah,
+				"product":      keranjang.Product,
+
+				"user": gin.H{
+					"id_user": keranjang.User.IDUser,
+					"nama":    keranjang.User.Nama,
+				},
+			},
+		})
+	})
+
 }
 
 
