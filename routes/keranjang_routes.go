2fa512e5fbb3bb57253ceecf8c45d5244626c97c
rioadityapratama <rioaditya444@gmail.com>
2025-04-15 14:35:50 +0800

    menambahkan  method PUT BY ID

--- routes/keranjang_routes.go
+++ routes/keranjang_routes.go
@@ -54,7 +54,7 @@ func Keranjang(router *gin.Engine, db *gorm.DB) {
 			},
 		})
 	})
-	
+
 	router.POST("/keranjang", func(c *gin.Context) {
 		var keranjang models.Keranjang
 		if err := c.ShouldBindJSON(&keranjang); err != nil {
@@ -71,6 +71,33 @@ func Keranjang(router *gin.Engine, db *gorm.DB) {
 				"jumlah":       keranjang.Jumlah,
 				"product":      keranjang.Product,
 
+				"user": gin.H{
+					"id_user": keranjang.User.IDUser,
+					"nama":    keranjang.User.Nama,
+				},
+			},
+		})
+	})
+
+	router.PUT("/keranjang/:id", func(c *gin.Context) {
+		var keranjang models.Keranjang
+		if err := db.First(&keranjang, "id_keranjang = ?", c.Param("id")).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"message": "Keranjang not found"})
+			return
+		}
+		if err := c.ShouldBindJSON(&keranjang); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
+		db.Save(&keranjang)
+	
+		c.JSON(http.StatusOK, gin.H{
+			"data": gin.H{
+				"id_keranjang": keranjang.IDKeranjang,
+				"id_product":   keranjang.IDProduct,
+				"id_user":      keranjang.IDUser,
+				"jumlah":       keranjang.Jumlah,
+				"product":      keranjang.Product,
 				"user": gin.H{
 					"id_user": keranjang.User.IDUser,
 					"nama":    keranjang.User.Nama,
