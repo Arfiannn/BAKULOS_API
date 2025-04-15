f5b8f4fc38898fb8cda14cce782270d447406be5
rioadityapratama <rioaditya444@gmail.com>
2025-04-15 14:32:53 +0800

    menambahkan  method GET

--- routes/keranjang_routes.go
+++ routes/keranjang_routes.go
@@ -9,7 +9,28 @@ import (
 )
 
 func Keranjang(router *gin.Engine, db *gorm.DB) {
-	
+	router.GET("/keranjang", func(c *gin.Context) {
+		var keranjang []models.Keranjang
+		db.Preload("Product").Preload("User").Find(&keranjang)
+
+		var result []gin.H
+		for _, keranjang := range keranjang {
+			result = append(result, gin.H{
+				"id_keranjang": keranjang.IDKeranjang,
+				"id_product":   keranjang.IDProduct,
+				"id_user":      keranjang.IDUser,
+				"jumlah":       keranjang.Jumlah,
+				"product":      keranjang.Product,
+				
+				"user": gin.H{
+					"id_user": keranjang.User.IDUser,
+					"nama":    keranjang.User.Nama,
+				},
+			})
+		}
+
+		c.JSON(http.StatusOK, gin.H{"data": result})
+	})
 }
 
 
