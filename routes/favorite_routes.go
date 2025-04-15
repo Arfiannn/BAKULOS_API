38f40520f97745e79f52eb7325fe828cf770ac30
Arfiannn <arfianpndg@gmail.com>
2025-04-15 15:41:30 +0800

    membuat endpoint GET /favorite/:id untuk menampilkan detail data favorit berdasarkan ID dengan relasi user dan produk

--- routes/favorite_routes.go
+++ routes/favorite_routes.go
@@ -39,4 +39,35 @@ func Favorite(router *gin.Engine, db *gorm.DB) {
 		c.JSON(http.StatusOK, gin.H{"data": result})
 	})
 
+	router.GET("/favorite/:id", func(c *gin.Context) {
+		var favorite models.Favorite
+		if err := db.Preload("User").Preload("Product").First(&favorite, "id_favorite = ?", c.Param("id")).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"message": "Favorite not found"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": gin.H{
+				"id_favorite": favorite.IDFavorite,
+				"id_product":  favorite.IDProduct,
+				"id_user":     favorite.IDUser,
+
+				"user": gin.H{
+					"nama":  favorite.User.Nama,
+					"email": favorite.User.Email,
+
+					"product": gin.H{
+						"id_product": favorite.Product.IDProduct,
+						"brand":      favorite.Product.Brand,
+						"kategori":   favorite.Product.Kategori,
+						"size":       favorite.Product.Size,
+						"price":      favorite.Product.Price,
+						"deskripsi":  favorite.Product.Deskripsi,
+						"image":      favorite.Product.Image,
+					},
+				},
+			},
+		})
+	})
+
 }
